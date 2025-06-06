81d9a04849427122c35e77a28b9e29d2b2ea99cb
John-1005 <jtgarza48@gmail.com>
2025-06-06 15:14:39 -0700

    Added posts table and create posts queries

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+CREATE TABLE posts(
+  id UUID PRIMARY KEY NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  title TEXT NOT NULL,
+  url TEXT UNIQUE,
+  description TEXT,
+  published_at TIMESTAMP,
+  feed_id UUID NOT NULL,
+  FOREIGN KEY (feed_id)
+  REFERENCES feeds(id)
+);
+
+
+-- +goose Down
+DROP TABLE posts;
