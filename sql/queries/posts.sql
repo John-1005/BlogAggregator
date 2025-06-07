6be27bbcfde36bd4e7450aea8654834d1c0ed0ac
John-1005 <jtgarza48@gmail.com>
2025-06-07 14:18:48 -0700

    Fixed some errors in the posts queries also started to update the scraper to save posts

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -7,7 +7,7 @@ RETURNING *;
 
 -- name: GetPostsForUser :many
 SELECT posts.*, feeds.name AS feed_name from posts
-JOIN feed_follows ON feed_follows.feed.id = posts.feed_id
+JOIN feed_follows ON feed_follows.feed_id = posts.feed_id
 JOIN feeds  ON posts.feed_id = feeds.id
 WHERE feed_follows.user_id = $1
 ORDER BY posts.published_at DESC
